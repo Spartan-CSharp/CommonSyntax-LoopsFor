32d6ec2ae40c25c2c3e58dc9d63a4e890c96a136
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:18:01 -0500

    Create Code
    Ask the user for a comma-separated list of first names (no spaces). Split the string into a string array. Loop through the array and tell each person hello.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Enter a comma-separated list of first names (no spaces): ");
+			string firstnamelist = Console.ReadLine();
+			string[] firstnames = firstnamelist.Split(',');
+
+			for ( int i = 0; i < firstnames.Length; i++ )
+			{
+				Console.WriteLine($"Hello {firstnames[i]}!");
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
